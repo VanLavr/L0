d503f200e655641523c9911cbfc67bbf1796c30c
VanLavr <lavr.i-n@mail.ru>
2024-02-25 11:38:40 +0300

    added cache and designed service

--- internal/pkg/err/err.go
+++ internal/pkg/err/err.go
@@ -4,4 +4,5 @@ import "errors"
 
 var (
 	ErrInvalidEnvironment = errors.New("provided environment is invalid")
+	ErrNoSuchKeyInCache   = errors.New("there no such key in cache")
 )
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,33 @@
 package service
+
+import (
+	"time"
+
+	"github.com/VanLavr/L0/internal/delivery/http"
+	"github.com/VanLavr/L0/model"
+)
+
+type service struct{}
+
+type Repository interface {
+	SaveOrder(*model.Order) string
+	GetOrder(string) *model.Order
+}
+
+type Cache interface {
+	Set(key string, value any, duration time.Duration)
+	Get(key string) any
+	Delete(key string) error
+}
+
+func New() http.Service {
+	return &service{}
+}
+
+func (s *service) SaveOrder(*model.Order) (string, error) {
+	panic("not implemented")
+}
+
+func (s *service) GetOrder(string) *model.Order {
+	panic("not implemented")
+}
